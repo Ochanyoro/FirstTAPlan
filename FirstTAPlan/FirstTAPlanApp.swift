f8e18257bba9a46166704089d9d859ca11337ebc
Ochanyoro <wadaoo0223@icloud.com>
2022-06-30 14:21:46 +0900

    Initial Commit

---
+++ FirstTAPlan/FirstTAPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTAPlanApp.swift
+//  FirstTAPlan
+//
+//  Created by 大和田一裕 on 2022/06/30.
+//
+
+import SwiftUI
+
+@main
+struct FirstTAPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
